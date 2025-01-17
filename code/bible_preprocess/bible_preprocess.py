0cbc8b27fc67d9be1cbc287963c6ab747aee4fc6
SteffRhes <steffrhes@pm.me>
2025-01-17 16:58:35 +0100

    modify bible_preprocess

--- code/bible_preprocess/bible_preprocess.py
+++ code/bible_preprocess/bible_preprocess.py
@@ -2,7 +2,7 @@ import os
 import re
 
 import nltk
-from nltk.tokenize import sent_tokenize
+import string
 
 
 IN_FILE = "/veld/input/" + os.getenv("in_file")
@@ -12,21 +12,28 @@ OUT_FILE = "/veld/output/" + os.getenv("out_file")
 # load sentence splitting function
 nltk.download('punkt')
 
-
+# read file
 with open(IN_FILE, "r") as f:
     content = f.read()
 
+# transform content
 content = content.lower()
 content = content.replace("\r\n", " ")
 content = content.replace("\n", " ")
 content = re.sub("\d+:\d+ ", "", content)
-content = sent_tokenize(content)
+sentence_list = []
+for sentence in nltk.sent_tokenize(content):
+    word_list_new = []
+    for word in nltk.word_tokenize(sentence):
+        if word not in string.punctuation:
+            word_list_new.append(word)
+    sentence_list.append(" ".join(word_list_new))
+
+# from line 29792 onwards, there is only gutenberg metadata left. So cut that
+sentence_list = sentence_list[:29792]
 
 # write to file, one sentence per line
 with open(OUT_FILE, "w") as f:
-    for line_nr, sentence in enumerate(content):
-        # from line 29792 onwards, there is only gutenberg metadata left, so break the loop
-        if line_nr == 29792:
-            break
+    for sentence in sentence_list:
         f.write(sentence + "\n")
 
